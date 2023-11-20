46e4919b3aac81b5c800a43c86b8b5fa719e4098
rahulandure <115336006+rahulandure@users.noreply.github.com>
2023-11-20 20:13:37 +0530

    Add files via upload

---
+++ src/components/discount/Dcard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Slider from "react-slick";
+import Ddata from "./Ddata";
+
+const Dcard = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true
+    };
+    return (
+        <>
+            <Slider {...settings}>
+                {Ddata.map((value, index) => {
+                    return (
+                        <>
+                        <div className="box product" key={index}>
+                            <div className="img">
+                                <img src={value.cover} alt="" width='100%'/>
+                            </div>
+                            <h4>{value.name}</h4>
+                            <span>{value.prize}</span>
+                        </div>
+                        </>
+                    )
+                })}
+            </Slider>
+        </>
+    )
+}
+
+export default Dcard
---
+++ src/components/discount/Ddata.js
@@ -0,0 +1,50 @@
+const Ddata = [
+    {
+        cover : "./images/discount/discount1.png",
+        name : "JBL Headphones",
+        prize : "₹ 7399"
+    },
+    {
+        cover : "./images/discount/discount2.png",
+        name : "PS6 Play Station",
+        prize : "₹ 9499"
+    },
+    {
+        cover : "./images/discount/discount3.png",
+        name : "CCTV Cameras",
+        prize : "₹ 3899"
+    },
+    {
+        cover : "./images/discount/discount4.png",
+        name : "Sony Speakers",
+        prize : "₹ 2699"
+    },
+    {
+        cover : "./images/discount/discount5.png",
+        name : "Samsung LED TV",
+        prize : "₹ 47299"
+    },
+    {
+        cover : "./images/discount/discount6.png",
+        name : "RG Products",
+        prize : "₹ 73499"
+    },
+    {
+        cover : "./images/discount/discount7.png",
+        name : "Apple Box",
+        prize : "₹ 67499"
+    },
+    {
+        cover : "./images/discount/discount8.png",
+        name : "Washing Machine",
+        prize : "₹ 32499"
+    },
+    {
+        cover : "./images/discount/discount9.png",
+        name : "Hair Dryer",
+        prize : "₹ 1399"
+    },
+    
+]
+
+export default Ddata
