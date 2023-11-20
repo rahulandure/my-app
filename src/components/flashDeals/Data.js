aa46d4d4f60559278a4697418b5bcf339fc1f97b
rahulandure <115336006+rahulandure@users.noreply.github.com>
2023-11-20 20:14:38 +0530

    Add files via upload

---
+++ src/components/flashDeals/Data.js
@@ -0,0 +1,50 @@
+const Data = {
+
+    productItems: [
+
+        {
+            id: 1,
+            discount: 50,
+            cover: "./images/flash/flash-1.png",
+            name: "shoes",
+            prize: "₹ 999"
+        },
+        {
+            id: 2,
+            discount: 40,
+            cover: "./images/flash/flash-2.png",
+            name: "Smart Watch",
+            prize: "₹ 1899"
+        },
+        {
+            id: 3,
+            discount: 50,
+            cover: "./images/flash/flash-3.png",
+            name: "Iphone Mobile Black",
+            prize: "₹ 130000"
+        },
+        {
+            id: 4,
+            discount: 40,
+            cover: "./images/flash/flash-4.png",
+            name: "Men's T-shirt",
+            prize: "₹ 450"
+        },
+        {
+            id: 5,
+            discount: 50,
+            cover: "./images/flash/flash-5.png",
+            name: "Headphones",
+            prize: "₹ 2450"
+        },
+        {
+            id: 6,
+            discount: 50,
+            cover: "./images/flash/flash-6.png",
+            name: "shoes",
+            prize: "₹ 1299"
+        },
+    ]
+}
+
+export default Data
