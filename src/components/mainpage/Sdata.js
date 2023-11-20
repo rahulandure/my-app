832bd7468890ee199873609c4bc42a356dab3434
rahulandure <115336006+rahulandure@users.noreply.github.com>
2023-11-20 20:15:33 +0530

    Add files via upload

---
+++ src/components/mainpage/Sdata.js
@@ -0,0 +1,28 @@
+const Sdata = [
+    {
+        id:1,
+        title : "50% Off For Your First Shopping",
+        desc : "Looking for a new pair of shoes? Get 50% OFF our latest selections. The sale ends this Sunday. Buy here: ",
+        cover : "./images/slide-card/slide-1.png",
+    },
+    {
+        id:2,
+        title : "50% Off For Your First Shopping",
+        desc : "The end-of-season sale is back. Get 50% off this week on all collections. Hurry! You don’t want to miss this opportunity!",
+        cover : "./images/slide-card/slide-2.png",
+    },
+    {
+        id:3,
+        title : "50% Off For Your First Shopping",
+        desc : "Looking for a Fashion Wear ? Get 50% OFF our latest selections. The sale ends this Sunday. Buy here: ",
+        cover : "./images/slide-card/slide-3.png",
+    },
+    {
+        id:4,
+        title : "50% Off For Your First Shopping",
+        desc : "The end-of-season sale is back. Get 50% off this week on all collections. Hurry! You don’t want to miss this opportunity!",
+        cover : "./images/slide-card/slide-4.png",
+    },
+]
+
+export default Sdata
