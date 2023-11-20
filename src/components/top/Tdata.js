69292deffa07ef4c173d326f8f6c45af1c3c31c1
rahulandure <115336006+rahulandure@users.noreply.github.com>
2023-11-20 20:18:26 +0530

    Add files via upload

---
+++ src/components/top/Tdata.js
@@ -0,0 +1,29 @@
+const Tdata = [
+    {
+        cover : "./images/top/category-1.png",
+        para : "headphone",
+        desc : "3k Orders this Week"
+    },
+    {
+        cover : "./images/top/category-2.png",
+        para : "watch",
+        desc : "4k Orders this Week"
+    },
+    {
+        cover : "./images/top/category-3.png",
+        para : "sunglasses",
+        desc : "6k Orders this Week"
+    },
+    {
+        cover : "./images/top/category-2.png",
+        para : "watch",
+        desc : "4k Orders this Week"
+    },
+    {
+        cover : "./images/top/category-3.png",
+        para : "sunglasses",
+        desc : "6k Orders this Week"
+    },
+]
+
+export default Tdata
---
+++ src/components/top/TopCate.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+import TopCart from "./TopCart";
+
+const TopCate = () => {
+    return (
+        <>
+            <section className="TopCate background">
+                <div className="container">
+                    <div className="heading d-flex">
+                        <div className="heading-left row f-flex">
+                            <i className="fa-solid fa-border-all"></i>
+                            <h2>Top Categories</h2>
+                        </div>
+                        <div className="heading-right row">
+                            <span>View All</span>
+                            <i className="fa-solid fa-caret-right"></i>
+                        </div>
+                    </div>
+                    <TopCart />
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default TopCate
